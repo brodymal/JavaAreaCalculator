356edc4ada046c4ec81caeb8dc6375847bb80437
Brody <133686891+brodymal@users.noreply.github.com>
2026-08-31 14:33:50 -0500

    Add files via upload
    Fixes styling to adhere to Google Java Style Guidelines.

--- AreaProgram.java
+++ AreaProgram.java
@@ -13,34 +13,35 @@ public class AreaProgram {
 			option = keyboard.nextInt();
 			
 			if (option < 1 || option > 4) {
-				System.out.println("Error, number chosen out of bounds. ");
-			} else 
-				switch (option) {
-					case 1:
-						// Case 1 finds area of a square using a = l^2. 
-						System.out.print("Area of a Square \nEnter the square's length: ");
-						double length = keyboard.nextDouble();
-						area = length * length;
-						System.out.println("The area of the square is: " + area);
-						break;
-					case 2:
-						// Case 2 finds area of circle using a = r^2 * pi.
-						System.out.print("Area of a Circle \nEnter the circles radius: ");
-						double radius = keyboard.nextDouble();
-						area = Math.pow(radius, 2) * Math.PI;
-						System.out.println("The area of the circle is " + area);
-						break;
-					case 3:
-						// Case 3 finds area of a triangle using a = bh/2.
-						System.out.print("Area of a Triangle \nEnter the triangles base: ");
-						double base = keyboard.nextDouble();
-						System.out.print("Area of a Triangle \nEnter the triangles height: ");
-						double height = keyboard.nextDouble();
-						area = base * height / 2.0;
-						System.out.println("The area of the triangle is " + area);
-						break;
+			  System.out.println("Error, number chosen out of bounds. ");
+			} else {
+			  switch (option) {
+			    case 1:
+				  // Case 1 finds area of a square using a = l^2. 
+				  System.out.print("Area of a Square \nEnter the square's length: ");
+				  double length = keyboard.nextDouble();
+				  area = length * length;
+				  System.out.println("The area of the square is: " + area);
+				  break;
+				case 2:
+				  // Case 2 finds area of circle using a = r^2 * pi.
+				  System.out.print("Area of a Circle \nEnter the circles radius: ");
+				  double radius = keyboard.nextDouble();
+				  area = Math.pow(radius, 2) * Math.PI;
+				  System.out.println("The area of the circle is " + area);
+				  break;
+				case 3:
+				  // Case 3 finds area of a triangle using a = bh/2.
+				  System.out.print("Area of a Triangle \nEnter the triangles base: ");
+				  double base = keyboard.nextDouble();
+				  System.out.print("Area of a Triangle \nEnter the triangles height: ");
+				  double height = keyboard.nextDouble();
+				  area = base * height / 2.0;
+				  System.out.println("The area of the triangle is " + area);
+				  break;
+			  }
 
-				}
+			}
 			
 
 		} while (option != 4);
