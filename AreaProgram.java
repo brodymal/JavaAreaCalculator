bcfdc8414ccba6c46cbbfa26f1b3638a6784fccf
Brody <133686891+brodymal@users.noreply.github.com>
2026-08-31 14:05:33 -0500

    Add files via upload
    The functionality for finding the area of the square has been completed.

--- AreaProgram.java
+++ AreaProgram.java
@@ -11,9 +11,18 @@ public class AreaProgram {
 			System.out.print("Enter the desired option[1-4]: ");
 			option = keyboard.nextInt();
 			
-			if(option < 1 || option > 4) {
+			if (option < 1 || option > 4) {
 				System.out.println("Error, number chosen out of bounds. ");
-			}
+			} else 
+				switch (option) {
+					case 1:
+						// Case 1 is the implementation for finding the area of a square.
+						System.out.print("Area of a Square \nEnter the square's length: ");
+						double length = keyboard.nextDouble();
+						double area = length * length;
+						System.out.println("The area of the square is: " + area);
+						break;
+				}
 			
 
 		} while (option != 4);
